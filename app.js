526ffec9702add144df5eb6a1ee4ea64ce8d4cf6
Bushra1995 <bushra95ga@gmail.com>
2023-04-18 01:59:40 +0300

    done

---
+++ app.js
@@ -0,0 +1,45 @@
+// Define the constructor function to create product objects
+function Product(title, price, description, image) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.image = image;
+}
+
+// Define the array to store the product objects
+const products = [];
+
+
+
+// Define the function to render the products in the main section
+function renderProducts() {
+    // Get a reference to the main section element
+    const main = document.querySelector('main');
+    // Use the map() method to create a card element for each product object
+    const cards = products.map(product => `
+      <div class="card">
+        <img src="${product.image}" alt="${product.title}">
+        <h2>${product.title}</h2>
+        <p>${product.description}</p>
+        <span>${product.price}</span>
+      </div>
+    `);
+    // Add the cards to the main section
+    main.innerHTML = cards.join('');
+}
+
+
+// Fetch data from API and create product objects
+fetch('https://example.com/products')
+    .then(response => response.json())
+    .then(data => {
+        // Create a new product object for each item in the data
+        data.forEach(item => {
+            const product = new Product(item.title, item.price, item.description, item.image);
+            // Push the new product object to the products array
+            products.push(product);
+        });
+        // Render the products in the main section
+        renderProducts();
+    })
+    .catch(error => console.error(error));
